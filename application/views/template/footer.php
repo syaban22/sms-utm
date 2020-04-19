1861cf24f29790f4eb89680f12156df47d714c7f
syaban <syabansim@gmail.com>
2020-04-19 22:49:42 +0700

    java method

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -79,6 +79,26 @@
 </script>
 
 <script>
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+</script>
+
+<script>
   $('#ktp').on('change', function() {
     //get the file name
     var fileName = $(this).val();
@@ -91,38 +111,6 @@
 
 <script src="<?= base_url('assets/'); ?>js/pop/js3.js"></script>
 <script src="<?= base_url('assets/'); ?>js/pop/minjs3.js"></script>
-
-<script type="text/javascript">
-  $(document).ready(function() {
-
-    $('.detail').click(function() {
-
-      var id = $(this).attr('relid'); //get the attribute value
-
-      $.ajax({
-        url: "<?php echo base_url(); ?>Mahasiswa/getData",
-        data: {
-          id: id
-        },
-        method: 'GET',
-        dataType: 'json',
-        success: function(response) {
-          $('#student_name').html(response.name); //hold the response in id and show on popup
-          $('#student_email').html(response.email);
-          $('#student_phone').html(response.phone);
-          $('#show_modal').modal({
-            backdrop: 'static',
-            keyboard: true,
-            show: true
-          });
-        }
-      });
-    });
-  });
-</script>
-
-
-
 </body>
 
 </html>
